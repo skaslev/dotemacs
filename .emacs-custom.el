00e0c827927f3f3ce30ce8b9b7758257101c6c36
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2026-01-21 13:58:08 +0200

    Minor

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,7 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company lsp-mode 2048-game evil-anzu anzu exec-path-from-shell asn1-mode auto-complete caml cmake-mode csharp-mode csproj-mode dockerfile-mode evil evil-leader evil-search-highlight-persist evil-surround glsl-mode go-mode google-c-style google-this haskell-mode idris-mode lean-mode lua-mode php-mode powershell protobuf-mode qml-mode qt-pro-mode rust-mode smart-compile tuareg typescript-mode yaml-mode zig-mode)))
+   '(2048-game anzu asn1-mode auto-complete caml cmake-mode company csharp-mode
+               csproj-mode dockerfile-mode evil evil-anzu evil-leader
+               evil-search-highlight-persist evil-surround exec-path-from-shell
+               flycheck glsl-mode go-mode google-c-style google-this
+               haskell-mode idris-mode lean-mode lsp-mode lua-mode php-mode
+               powershell protobuf-mode qml-mode qt-pro-mode rust-mode
+               smart-compile tuareg typescript-mode yaml-mode zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
